b62595a7978fd75a420d6e6ee7d22e4292240a56
Suresh <suris6530@gmail.com>
2026-07-18 15:30:45 +0530

    feat: initialize MongoDB connection with Mongoose and timeout configurations

--- models/db.js
+++ models/db.js
@@ -2,12 +2,13 @@ const mongoose = require("mongoose");
 
 
 const mongo_url = process.env.MONGO_URI;
+mongoose.set('bufferTimeoutMS', 30000);
+
 mongoose
   .connect(mongo_url, {
     serverSelectionTimeoutMS: 30000, // 30 seconds
     socketTimeoutMS: 45000, // 45 seconds
     connectTimeoutMS: 30000, // 30 seconds
-    bufferTimeoutMS: 30000, // 30 seconds
   })
   .then(() => {
     console.log("MongoDB Connected");
